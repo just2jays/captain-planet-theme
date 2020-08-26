4e307bbc39eeb9d6950e5d0830a24ee778970e5a
JJ <jguaragno@mansueto.com>
2020-08-26 13:38:27 -0400

    The start of the Captain Planet theme code

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// javascript
+
+class Captain {
+  init{
+    name: ''; // "Planet"
+    descriptor: ''; // "A hero",
+  }
+  
+  take(object){
+    // takes polluttion // a num + 1
+    if object.name == 'polution' {
+      return 0;
+    }
+  } 
+}
+
+let captain;
+let pollution = 100;
+
+captain.name = 'Planet';
+captain.description = 'A hero';
+
+captain.take = ({  }) => {
+	
+}
+
+
+console.log(
+  ``;
+)
+
+
+// just riffing some ideas here lol;
